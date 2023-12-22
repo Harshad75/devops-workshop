cf8d66fd57472cc24e871d8604373ab239663c01
HarshadShaik786 <harshad.shaik904@gamil.com>
2023-12-22 15:24:16 +0530

    Added setup of eks

---
+++ my-terraform-scripts/eks/variables.tf
@@ -0,0 +1,12 @@
+variable "sg_ids" {
+type = string
+}
+
+variable "subnet_ids" {
+  type = list
+}
+
+variable "vpc_id" {
+   //default="vpc-0c3a8bc8dccf299b7"
+   type = string
+}
---
+++ my-terraform-scripts/sg_eks/variables.tf
@@ -0,0 +1,4 @@
+variable "vpc_id" {
+   //default = "vpc-0c3a8bc8dccf299b7"
+   type = string
+}
